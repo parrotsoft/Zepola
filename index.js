8de3bbf411ca907743850001d32441f830e5e142
Miguel Lopez <lopezarizamiguel@gmail.com>
2019-10-21 17:02:56 -0500

    Feat: creando primera pregunta en inquirer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const clear = require('clear');
 const figlet = require('figlet');
 const chalk = require('chalk');
+const inquirer = require('inquirer');
 
 clear();
 console.log(
@@ -10,4 +11,25 @@ console.log(
         figlet.textSync('Zepola', { horizontalLayout: 'full'})
     )
 )
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'proyecto',
+        message: 'Que lenguaje utilizamos',
+        choices: ['Angular','Laravel','Python']
+    }
+]).then(answers => {
+    switch(answers.proyecto) {
+        case 'Angular':
+            console.log('Crearemos un proyecto de Angular...')
+            break;
+        case 'Laravel':
+                console.log('Crearemos un proyecto de Laravel...')
+            break;
+        case 'Python':
+            console.log('Crearemos un proyecto de Python...')
+            break;
+    }
+})
 
